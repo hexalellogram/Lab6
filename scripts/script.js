677a3a0f739ca8779c87e56aee30bb0fe2fb0052
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-05-07 02:49:32 -0700

    add the loop body for each entry

--- scripts/script.js
+++ scripts/script.js
@@ -16,19 +16,27 @@ document.addEventListener('DOMContentLoaded', () => {
    * the current entry for each journal-entry element.
    */
 
-  let url = ""  // SET URL 
+  let url = "https://cse110lab6.herokuapp.com/entries"  // SET URL 
 
   fetch(url)
-    .then( /* FILL IN RESPONSE HANDLING HERE */ )
+    .then( response => response.json() )
     .then(entries => {
       entries.forEach((entry) => {
        
         let newPost;  
 
 
-        // CODE GOES HERE vvv
+        // get the main
+        let main = document.getElementsByTagName('main')[0];
         
+        // create new entry
+        newPost = document.createElement('journal-entry');
 
+        // set the different qualities of each entry
+        newPost.entry = entry;
+
+        // append to main
+        main.appendChild(newPost);
 
 
 
